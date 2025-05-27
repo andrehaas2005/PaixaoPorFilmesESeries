3b6df27e23f62564042041bedea58c6f31a71ae6
Andre Haas <andrehaas2005@gmail.com>
2025-05-27 20:05:30 -0300

    Initial Commit

---
+++ PaixaoPorFilmesESeries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaixaoPorFilmesESeries
+//
+//  Created by Andre  Haas on 27/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
